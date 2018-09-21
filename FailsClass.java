49b2e546903cd70b88201ccfba8df7dc3979e519
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2018-09-21 02:06:55 -0500

    added class failed/class needed for major

---
+++ FailsClass.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package se;
+
+/**
+ * @author Erik Ayavaca-Tirado
+ *
+ */
+// Can also be used as which classes a user needs to take 
+public class FailsClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	// Takes in the user's transcript
+	// Takes in user's major course list
+	// Compares both lists 
+	// Puts classes not already taken into an array
+	// outputs classes as " you still need to complete these"
+	// Goes back into Transcript and looks for classes with F,D, 1.5,1,.5 or 0 
+	// these classes are put into list or array
+	// outputs to user as " need to retake this"
+	// Output is returned to user 
+
+}
