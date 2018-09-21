dd15e58d7046e08ec3549e526f36b3c179f02c05
Jackson Nelson <nelsonj6@augsburg.edu>
2018-09-21 14:55:10 -0500

    Updated TU

--- TranscriptUpload.java
+++ TranscriptUpload.java
@@ -8,27 +8,15 @@ import java.io.*;
 
 public class TranscriptUpload {
 
-	public void uploadFile()
+	public static void uploadFile()
 	{
 		String filename = null;
-		try
-		{
-			Frame window = null;
-			FileDialog dialog = new FileDialog(window, "Choose a File", FileDialog.LOAD);
-			dialog.setVisible(true);
-			filename = dialog.getName();
-		}
-		finally
-		{
-			System.out.println(filename);
-		}
-	}
 	
-	public static void main(String[] args)
-	{
-		uploadFile()
+		Frame window = null;
+		FileDialog dialog = new FileDialog(window, "Choose a File", FileDialog.LOAD);
+		dialog.setVisible(true);
+		filename = dialog.getFile();
+		
+		System.out.println(filename);
 	}
-
 }
-
-
