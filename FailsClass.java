8307be023c584b51ef9dd3944e799fa63a996367
Chase Strickler <stricklc@augsburg.edu>
2018-09-28 20:11:47 -0500

    Revert "Added fillAbbrev method to fill the hashmap with abbreviations manually"
    This reverts commit daaeea5d460aedfb5b2e3c0a3b34c517471be2b4.

--- FailsClass.java
+++ FailsClass.java
@@ -4,11 +4,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.io.*;
-import java.util.*;
-import java.util.*;
 
 /**
  * @author Erik Ayavaca-Tirado
@@ -31,13 +27,8 @@ public class FailsClass {
 
 	public static void main(String[] args) throws IOException {
 		//Replace with own file paths when testing
-		//Path firstFile = Paths.get("/Users/erikayavaca/Desktop/workspace/CourseTest/src/Courselist.txt");
-		//Path secondFile = Paths.get("/Users/erikayavaca/Desktop/workspace/CourseTest/src/Transcript.txt");
-		String args1 = args[0];
-		String args2 = args[1];
-		Map<String, Integer> abbreviations = new HashMap<>();
-		Map<String, Integer> courses = new HashMap<>();
-		fillAbbrev(abbreviations);
+		Path firstFile = Paths.get("/Users/erikayavaca/Desktop/workspace/CourseTest/src/Courselist.txt");
+		Path secondFile = Paths.get("/Users/erikayavaca/Desktop/workspace/CourseTest/src/Transcript.txt");
 		List<String> firstFileContent = Files.readAllLines(firstFile, Charset.defaultCharset());
 		List<String> secondFileContent = Files.readAllLines(secondFile, Charset.defaultCharset());
 		System.out.println("Still need these course: " + diffFiles(firstFileContent, secondFileContent));
@@ -53,74 +44,5 @@ public class FailsClass {
 		}
 		return diff;
 	}
-
-	/*
-	* This function manually adds all of the available abbreviations from the
-	* university to cross check with the file we are reading in
-	* @param hm is the hashmap for the abbreviated courses in the university's catalog
-	*/
-	public static void fillAbbrev(Map<String, Integer> hm) {
-		hm.put(CSC, 0);
-		hm.put(MAT, 0);
-		hm.put(ENL, 0);
-		hm.put(AIS, 0);
-		hm.put(COM, 0);
-		hm.put(PSY, 0);
-		hm.put(ANT, 0);
-		hm.put(CCS, 0);
-		hm.put(SOC, 0);
-		hm.put(THR, 0);
-		hm.put(WST, 0);
-		hm.put(POL, 0);
-		hm.put(ART, 0);
-		hm.put(HIS, 0);
-		hm.put(ECO, 0);
-		hm.put(PHI, 0);
-		hm.put(BUS, 0);
-		hm.put(FIN, 0);
-		hm.put(BIO, 0);
-		hm.put(HPE, 0);
-		hm.put(EDC, 0);
-		hm.put(EED, 0);
-		hm.put(CHM, 0);
-		hm.put(SPE, 0);
-		hm.put(SWK, 0);
-		hm.put(ENV, 0);
-		hm.put(PHY, 0);
-		hm.put(HON, 0);
-		hm.put(KEY, 0);
-		hm.put(MIS, 0);
-		hm.put(MKT, 0);
-		hm.put(HUM, 0);
-		hm.put(RLN, 0);
-		hm.put(THR, 0);
-		hm.put(MUS, 0);
-		hm.put(SPA, 0);
-		hm.put(MUE, 0);
-		hm.put(ACC, 0);
-		hm.put(SCI, 0);
-		hm.put(NUR, 0);
-		hm.put(NMS, 0);
-		hm.put(FLM, 0);
-		hm.put(THP, 0);
-		hm.put(URB, 0);
-		hm.put(YST, 0);
-
-	}
-
-	/*
-    * This function uses a TreeMap to sort the hashmap alphabetically
-    * @param Map <K,V> hm is the hashmap to be sorted
-    */ 
-    public static TreeMap sortByKey(Map<String, Integer> hm) {
-
-        // Create a new TreeMap called sorted
-        TreeMap<String, Integer> sorted = new TreeMap<>();
-
-        // Copy data from the hash map into the TreeMap, which will sort the map alphabetically
-        sorted.putAll(hm);
-        return(sorted);
-
-    }
 
 }
