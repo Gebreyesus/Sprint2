028800b74d85ad65d9807ea8b863264a47d37a22
wowbob396 <wowbob396@gmail.com>
2018-10-24 14:22:31 -0500

    removed old project and put into a folder

--- bin/callScrape.py
+++
@@ -1,7 +0,0 @@
-from mathematicians import simple_get
-website = input("What is the URL of the degree requirments page of your university?: ")
-type(website)
-raw_html = simple_get(website)
-len(raw_html)
-f = open("degree.html", "wb")
-f.write(raw_html)
