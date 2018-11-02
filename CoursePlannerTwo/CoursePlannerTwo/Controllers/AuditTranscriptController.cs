379ab8c4588709f70a5333cf693646d2213b2b91
wowbob396 <wowbob396@gmail.com>
2018-11-02 14:47:07 -0500

    created new view, and auditTranscript controller

---
+++ CoursePlannerTwo/CoursePlannerTwo/Controllers/AuditTranscriptController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace CoursePlannerTwo.Controllers
+{
+    public class AuditTranscriptController : Controller
+    {
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public string Test()
+        {
+            return HtmlEncoder.Default.Encode("Hello");
+        }
+    }
+}
