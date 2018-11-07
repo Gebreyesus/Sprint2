b9352b3caa4474756c48a8bcd1484d1b950b251c
wowbob396 <wowbob396@gmail.com>
2018-11-07 13:33:55 -0600

    created updated controller to work with the database

---
+++ CoursePlannerTwo/CoursePlannerTwo/Models/CatalogContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoursePlannerTwo.Models
+{
+    public class CatalogContext : DbContext
+    {   
+
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> courses { get; set; }
+    }
+
+    public class Course
+    {
+        public int id { get; set; }
+        public int courseNumber { get; set; }
+        public string title { get; set; }
+        public string titleShort { get; set; }
+        public string description { get; set; }
+        public List<Course> prereqs;
+
+      
+    }
+}
