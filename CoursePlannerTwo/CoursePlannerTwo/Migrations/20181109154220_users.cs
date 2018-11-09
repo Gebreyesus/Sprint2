d2873a2c2963ef59ff9ce6eaf7df2130684790dc
wowbob396 <wowbob396@gmail.com>
2018-11-09 11:05:43 -0600

    updated seedData class

---
+++ CoursePlannerTwo/CoursePlannerTwo/Migrations/20181109154220_users.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoursePlannerTwo.Migrations
+{
+    public partial class users : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "transcriptId",
+                table: "courses",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "transcriptId",
+                table: "courses");
+        }
+    }
+}
---
+++ CoursePlannerTwo/CoursePlannerTwo/Models/SeedData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoursePlannerTwo.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new CatalogContext(serviceProvider.GetRequiredService<DbContextOptions<CatalogContext>>()))
+            {
+                if (context.courses.Any())
+                {
+                    return; // db has been seeded
+                }
+
+            }
+        }
+    }
+}
